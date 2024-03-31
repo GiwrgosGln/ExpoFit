f96c6a23b016fce657bf88fa8d37fa9f252cfa85
Giwrgos.Gln <gergegl1999@gmail.com>
2024-03-31 18:06:05 +0300

    fix bug that navigates back to login screen with shortcuts

--- app/screens/main/HomeScreen.js
+++ app/screens/main/HomeScreen.js
@@ -1,10 +1,22 @@
-import React from "react";
-import { StyleSheet, Text, View, ScrollView } from "react-native";
+import React, { useEffect } from "react";
+import { StyleSheet, Text, View, BackHandler, ScrollView } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import globalStyles from "../../styles/globalStyles";
 import WorkoutList from "../../components/Workout/WorkoutList";
 
 const Home = () => {
+  // Disable back button listener when the component mounts
+  useEffect(() => {
+    const disableBackButton = () => {
+      return true;
+    };
+
+    BackHandler.addEventListener("hardwareBackPress", disableBackButton);
+
+    return () => {
+      BackHandler.removeEventListener("hardwareBackPress", disableBackButton);
+    };
+  }, []);
   return (
     <View style={globalStyles.container}>
       <StatusBar backgroundColor="#161a22" color="white" style="light" />
