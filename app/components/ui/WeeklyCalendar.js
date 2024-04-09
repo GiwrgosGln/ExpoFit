625e7bb45ad10a774e64903fe9caa4b5408144c0
GiwrgosGln <gergegl1999@gmail.com>
2024-04-09 14:24:14 +0300

    fix bug with dates

--- app/components/ui/WeeklyCalendar.js
+++ app/components/ui/WeeklyCalendar.js
@@ -4,14 +4,21 @@ import { View, Text, TouchableOpacity, FlatList } from "react-native";
 const WeeklyCalendar = ({ setSelectedDate }) => {
   const days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
   const currentDate = new Date();
-  const currentDayIndex = (currentDate.getDay() + 6) % 7;
+  const currentDayIndex = currentDate.getDay();
+  const currentMondayDate = new Date(currentDate);
+  const offset =
+    currentMondayDate.getDate() -
+    currentDayIndex +
+    (currentDayIndex === 0 ? -6 : 1);
+  currentMondayDate.setDate(offset);
   const [activeDay, setActiveDay] = useState(days[currentDayIndex]);
 
   const handleDayPress = (day) => {
     setActiveDay(day);
-    const selectedDate = new Date(currentDate);
-    const daysUntilSelectedDay = (days.indexOf(day) - currentDayIndex + 7) % 7;
-    selectedDate.setDate(currentDate.getDate() + daysUntilSelectedDay);
+    const selectedDate = new Date(currentMondayDate);
+    const daysUntilSelectedDay =
+      (days.indexOf(day) - days.indexOf("Mon") + 7) % 7;
+    selectedDate.setDate(currentMondayDate.getDate() + daysUntilSelectedDay);
     setSelectedDate(selectedDate);
     console.log(`You pressed ${day}`);
   };
@@ -24,35 +31,43 @@ const WeeklyCalendar = ({ setSelectedDate }) => {
         data={days}
         nestedScrollEnabled={true}
         keyExtractor={(item, index) => index.toString()}
-        renderItem={({ item, index }) => (
-          <TouchableOpacity
-            key={index}
-            style={{
-              alignItems: "center",
-              backgroundColor: activeDay === item ? "#6879f8" : "#292a3e",
-              paddingHorizontal: 12,
-              paddingVertical: 8,
-              borderRadius: 8,
-              marginRight: 10,
-              width: 60,
-            }}
-            onPress={() => handleDayPress(item)}
-          >
-            <Text style={{ fontSize: 16, fontWeight: "bold", color: "white" }}>
-              {item}
-            </Text>
-            <Text
+        renderItem={({ item, index }) => {
+          const dayIndex = (currentDayIndex + index) % 7;
+          const date = new Date(currentMondayDate);
+          date.setDate(currentMondayDate.getDate() + index);
+
+          return (
+            <TouchableOpacity
+              key={index}
               style={{
-                fontSize: 20,
-                marginTop: 5,
-                color: "white",
-                fontWeight: "700", // Changed to string since React Native doesn't support numeric fontWeight
+                alignItems: "center",
+                backgroundColor: activeDay === item ? "#6879f8" : "#292a3e",
+                paddingHorizontal: 12,
+                paddingVertical: 8,
+                borderRadius: 8,
+                marginRight: 10,
+                width: 60,
               }}
+              onPress={() => handleDayPress(item)}
             >
-              {currentDate.getDate() + index}
-            </Text>
-          </TouchableOpacity>
-        )}
+              <Text
+                style={{ fontSize: 16, fontWeight: "bold", color: "white" }}
+              >
+                {item}
+              </Text>
+              <Text
+                style={{
+                  fontSize: 20,
+                  marginTop: 5,
+                  color: "white",
+                  fontWeight: "700",
+                }}
+              >
+                {date.getDate()}
+              </Text>
+            </TouchableOpacity>
+          );
+        }}
         contentContainerStyle={{
           paddingVertical: 10,
         }}
