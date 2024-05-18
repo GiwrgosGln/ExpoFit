189bc49ae154a7cb22758eb838367caa240fe05a
GiwrgosGln <gergegl1999@gmail.com>
2024-05-18 11:39:38 +0300

    update sample workouts

--- app/components/Workout/SampleWorkouts.js
+++ app/components/Workout/SampleWorkouts.js
@@ -1,49 +1,62 @@
 import React from "react";
-import { View, Text, ScrollView } from "react-native";
-
-const workouts = [
-  { id: "1", title: "Upper", difficulty: "Beginner" },
-  { id: "2", title: "Lower", difficulty: "Beginner" },
-  { id: "3", title: "Push", difficulty: "Intermediate" },
-  { id: "4", title: "Pull", difficulty: "Intermediate" },
-  { id: "5", title: "Legs", difficulty: "Intermediate" },
-  { id: "6", title: "5x5", difficulty: "Intermediate" },
-];
-
-const WorkoutItem = ({ title, difficulty }) => (
-  <View
-    style={{
-      backgroundColor: "#6879f8",
-      padding: 20,
-      marginVertical: 8,
-      marginHorizontal: 8,
-      borderRadius: 10,
-      width: 150,
-    }}
-  >
-    <Text style={{ fontSize: 20, fontWeight: "bold", color: "white" }}>
-      {title}
-    </Text>
-    <Text style={{ fontSize: 14, color: "white" }}>{difficulty}</Text>
-  </View>
-);
+import {
+  View,
+  Text,
+  ScrollView,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
+import { useNavigation } from "@react-navigation/core";
+import workoutsData from "../../../data/routines.json";
 
 const SampleWorkouts = () => {
+  const navigation = useNavigation();
+
+  const handleStartWorkout = (routine) => {
+    navigation.navigate("WorkoutDetails", { routine });
+    console.log(routine);
+  };
+
   return (
     <ScrollView
       horizontal
       showsHorizontalScrollIndicator={false}
       nestedScrollEnabled={true}
     >
-      {workouts.map((item) => (
-        <WorkoutItem
-          key={item.id}
-          title={item.title}
-          difficulty={item.difficulty}
-        />
+      {workoutsData.workouts.map((routine, index) => (
+        <TouchableOpacity
+          key={index} // Ensure each item has a unique key
+          onPress={() => handleStartWorkout(routine)}
+          style={styles.workoutItem}
+        >
+          <Text style={styles.workoutTitle}>{routine.title}</Text>
+          <Text style={styles.workoutDetails}>
+            {routine.exercises.length} exercises
+          </Text>
+        </TouchableOpacity>
       ))}
     </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  workoutItem: {
+    backgroundColor: "#6879f8",
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 8,
+    borderRadius: 10,
+    width: 150,
+  },
+  workoutTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "white",
+  },
+  workoutDetails: {
+    fontSize: 14,
+    color: "white",
+  },
+});
 
 export default SampleWorkouts;
